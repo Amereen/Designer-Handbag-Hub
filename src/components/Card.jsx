cbffd2b6f502034eb1113981e7da8d8034c9abd6
Amereen Hanvel <amereenhanvel786@gmail.com>
2023-01-10 16:52:31 -0500

    created div className of card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
 const Card = ({ name, price, imageUrl, description, handleClick, addBtn }) => {
+  return (
+    <div className="card">
+
+    </div>
+  );
+};
+
+export default Card;
 
 
-
-
-
-}
