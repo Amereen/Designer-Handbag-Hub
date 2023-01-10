f3ef43a2e5a7cddeeaa8fa01050c9786fae18a87
Amereen Hanvel <amereenhanvel786@gmail.com>
2023-01-10 16:57:39 -0500

    created div className to store data;such as bagName and Bag price

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const Card = ({ name, price, imageUrl, description, handleClick, addBtn }) => {
   return (
     <div className="card">
-
+      <div className="card-data">
+        {/* <p className="name">{name}</p>
+        <p className="price">${price}</p> */}
+      </div>
     </div>
   );
 };
