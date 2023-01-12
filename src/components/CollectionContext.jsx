fbf72b2e8af9e0d8613e99d2a10aac6f7a323934
Amereen Hanvel <amereenhanvel786@gmail.com>
2023-01-12 00:23:29 -0500

    created func collection prov passed children and inputed state

--- src/components/CollectionContext.jsx
+++ src/components/CollectionContext.jsx
@@ -1,2 +1,15 @@
 import React, { createContext, useState } from "react";
 export const Collection = createContext();
+
+
+const CollectionProvider = ({ children }) => {
+  const [myCollection, setMyCollection] = useState([]);
+
+  return (
+    <Collection.Provider value={{ myCollection, setMyCollection }}>
+      {children}
+    </Collection.Provider>
+  );
+};
+
+export default CollectionProvider;
