24d537ef2223b683a8f9adbef68eed3d337910fa
Amereen Hanvel <amereenhanvel786@gmail.com>
2023-01-12 00:53:50 -0500

    remove from coll

--- src/components/MyCollection.jsx
+++ src/components/MyCollection.jsx
@@ -1,3 +1,13 @@
 import React, { useContext } from "react";
 import { Collection } from "./CollectionContext";
 import Card from "./Card";
+
+
+const MyCollection = () => {
+  const { myCollection, setMyCollection } = useContext(Collection);
+
+  const removeItem = (id) => {
+    const updatedCollection = myCollection.filter((item) => item.id !== id);
+    setMyCollection(updatedCollection);
+  };
+}  
