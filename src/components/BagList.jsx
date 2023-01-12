4d21c38ddf1ff9927576d4ec6ab062af82ffcd85
Amereen Hanvel <amereenhanvel786@gmail.com>
2023-01-12 00:48:53 -0500

    returned card comp w props and func

--- src/components/BagList.jsx
+++ src/components/BagList.jsx
@@ -42,6 +42,32 @@ const addItem = (id, name, price, description, image) => {
 if (loading) return <div>Loading...</div>;
 
 
+return (
+  <div className="baglist-container">
+    <div className="baglist-cards-con">
+      {luxuryBags.map(({ id, price, name, description, image }) => {
+        // is there a way to just map over state here^ instead of putting object again?
+        return (
+          <Card
+            key={id}
+            price={price}
+            name={name}
+            description={description}
+            imageUrl={image}
+            handleClick={() => {
+              addItem(id, name, price, description, image);
+            }}
+            addBtn={true}
+          // how did we get acess to addBtn?
+          />
+        );
+      })}
+    </div>
+  </div>
+);
+
+
+export default BagList;
 
 
 
@@ -50,4 +76,3 @@ if (loading) return <div>Loading...</div>;
 
 
 
-  }
