50fc0e10e880ef0d7456935f87a0c53bedcebc88
Amereen Hanvel <amereenhanvel786@gmail.com>
2023-01-12 00:25:00 -0500

    used useffect w fetch and empty [] to have it render once

--- src/components/BagList.jsx
+++ src/components/BagList.jsx
@@ -5,4 +5,17 @@ import { Collection } from "./CollectionContext";
 const BagList = () => {
   const [luxuryBags, setLuxuryBags] = useState([]);
   const [loading, setLoading] = useState(true);
+  const { setMyCollection } = useContext(Collection);
+  useEffect(() => {
+    fetch("http://localhost:8000/bags")
+      .then((response) => {
+        if (response.ok) return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+
+        setLuxuryBags(data);
+        setLoading(false);
+      });
+  }, []);
 }
