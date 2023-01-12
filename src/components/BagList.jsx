273740a8071c218f8d2c0bb4debb3ec482309a87
Amereen Hanvel <amereenhanvel786@gmail.com>
2023-01-12 00:15:09 -0500

    created BagList comp w states

--- src/components/BagList.jsx
+++ src/components/BagList.jsx
@@ -1,3 +1,8 @@
 import React, { useState, useEffect, useContext } from "react";
 import Card from "./Card";
 import { Collection } from "./CollectionContext";
+
+const BagList = () => {
+  const [luxuryBags, setLuxuryBags] = useState([]);
+  const [loading, setLoading] = useState(true);
+}
