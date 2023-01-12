a94b4dec52808063f05048c23911eb4d585ae388
Amereen Hanvel <amereenhanvel786@gmail.com>
2023-01-12 00:49:46 -0500

    imp navbar and created Header comp

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import NavBar from "./NavBar";
+
+function Header() {
+  return (
+    <header className="header">
+      <h5 className="brand">Designer Bags</h5>
+      <NavBar />
+    </header>
+  );
+}
+
+export default Header;
